061228f183ad81f1b2b52535ea1df04a3f852118
maverickhb <hasbeker@gmail.com>
2023-01-26 00:32:18 +0100

    random fruits add app example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Fruits from "./Fruits";
+import FruitsCounter from "./FruitsCounter";
+
+
 
 function App() {
+
+  const [fruits, setFruits] = React.useState([
+    { fruitName: 'banana', id: 1 },
+    { fruitName: 'apple', id: 2 },
+    { fruitName: 'plum', id: 3 },
+  ]);
+
+  // const fruitsArray = [
+  //   { fruitName: "orange", id: 4 },
+  //   { fruitName: "grapes", id: 5 },
+  //   { fruitName: "strawberry", id: 6 },
+  //   { fruitName: "mango", id: 7 },
+  //   { fruitName: "pineapple", id: 8 },
+  //   { fruitName: "pear", id: 9 },
+  //   { fruitName: "kiwi", id: 10 },
+  //   { fruitName: "watermelon", id: 11 },
+  //   { fruitName: "peach", id: 12 },
+  //   { fruitName: "lemon", id: 13 }
+  // ];
+  //const fruitsArray = ["orange", "grapes", "strawberry", "mango", "pineapple", "pear", "kiwi", "watermelon", "peach", "lemon"].map((fruit, idx) => ({fruitName: fruit, id: idx+4}));
+  const fruitsArray = ["orange", "grapes", "strawberry", "mango", "pineapple", "pear", "kiwi", "watermelon", "peach", "lemon"];
+
+
+  // function addFruit() {
+  //   // setFruits([{ fruitName: "orange", id: 4 }, ...fruits]);
+  //   //const randomIndex = Math.floor(Math.random() * fruitsArray.length);
+  //  // const newFruit = fruitsArray[randomIndex];
+  //  // setFruits([newFruit, ...fruits]);
+
+  //  if (fruitsArray.length === 0) {
+  //   alert("there is no more fruit in the basket");
+  // } else {
+  //   const randomIndex = Math.floor(Math.random() * fruitsArray.length);
+  //   //const newFruit = { fruitName: fruitsArray[randomIndex], id: randomIndex+4};
+  //   const newFruit = fruitsArray[randomIndex];
+  //   fruitsArray.splice(randomIndex, 1);
+  //   setFruits([newFruit, ...fruits]);
+    
+  // }
+  // }
+  function addFruit() {
+    if (fruitsArray.length === 0) {
+      alert("there is no more fruit in the basket");
+    } else {
+      const randomIndex = Math.floor(Math.random() * fruitsArray.length);
+      //const newFruit = { fruitName: fruitsArray[randomIndex], id: randomIndex + 4 };
+      const newFruit = { fruitName: fruitsArray[randomIndex], id: Date.now() + fruitsArray.length + randomIndex };
+      //fruitsArray.splice(randomIndex, 1);
+      setFruits([...fruits, newFruit]);
+      
+    }
+  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Where should the state go?</h1>
+      <Fruits fruits={fruits} />
+      <FruitsCounter fruitsCount={fruits} />
+      {/* <button onClick={addFruit}>Add Orange Fruit</button> */}
+      <button className="add-orange-fruit-button" onClick={addFruit}>Add a Random Fruit</button>
+
     </div>
+
   );
-}
+};
 
 export default App;
---
+++ src/FruitsCounter.js
@@ -0,0 +1,8 @@
+
+function FruitsCounter(props) {
+    return (
+        <h2>Total fruits: {props.fruitsCount.length}</h2>
+    )
+}
+
+export default FruitsCounter;
